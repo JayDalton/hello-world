0b48e6cc6ac4b1638f1297844b4ced78876e36ec
Thomas G <github@t-jay.net>
2017-05-08 21:50:48 +0200

    added scaffolding dialog menu

--- main.rs
+++ main.rs
@@ -1,3 +1,6 @@
+use std::io;
+use std::cmp::Ordering;
+
 fn simple_sieve(limit: usize) -> Vec<usize> {
     let mut is_prime = vec![true; limit+1];
     is_prime[0] = false;
@@ -18,7 +21,56 @@ fn simple_sieve(limit: usize) -> Vec<usize> {
         .collect()
 }
 
+fn dialog_1() {
+    println!("Bitte geben sie eine gerade ganze Zahl n > 2 ein: ");
+
+    println!("Es gibt ... Darstellungen von .... als Summe zweier Zahlen.");
+}
+
+fn dialog_2(){
+    println!("Bitte geben sie eine gerade ganze Zahl n > 2 ein: ");
+    println!("Bitte geben sie eine gerade ganze Zahl m > ... ein: ");
+}
+
+fn dialog_3(){
+    println!("Bitte geben sie eine gerade ganze Zahl n > 2 ein: ");
+}
+
 fn main() {
-    println!("Hello, world!");
-    println!("{:?}", simple_sieve(10000));
+
+    loop {
+
+        println!("");
+        println!("Bitte wählen:");
+        println!("");
+        println!("(1) Anzahl der Goldbach-Zerlegungen von n");
+        println!("(2) Anzahl der Goldbach-Zerlegungen nach Zahlen von n bis m");
+        println!("(3) Alle Goldbach-Zerlegungen von n");
+        println!("(4) Programm beenden");
+        println!("");
+        println!("Ihre Wahl: ");
+        
+        let mut select = String::new();
+
+        io::stdin().read_line(&mut select).expect("Failed to read line!");
+
+        let select: u32 = match select.trim().parse()
+        {
+            Ok(num) => num,
+            Err(_) => continue
+        };
+
+        println!("You selected: {}", select);
+
+        match select {
+            1 => dialog_1(),
+            2 => println!(" _2_ "),
+            3 => println!(" _3_ "),
+            4 => break,
+            _ => println!(" unknown ")
+        }
+    }
+
+
+    //println!("{:?}", simple_sieve(100));
 }
